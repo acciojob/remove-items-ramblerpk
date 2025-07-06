72615be0fe1c34eee5b94c5f336034848cfa4b31
Piyush Kumar <109018436+ramblerpk@users.noreply.github.com>
2025-07-06 20:34:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click", ()=>{
+	let select = document.getElementById("colorSelect");
+	let color = select.value;
+
+	for(let i=0;i<select.options.length;i++){
+		if(select.options[i].value===color){
+			select.remove(i);
+			break;
+		}
+		
+	}
+});
